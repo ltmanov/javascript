2bd1efaef9f9d9f8be65df6be0d0623df44d487a
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-05 08:28:31 -0500

    a

--- angular/angular.php
+++ angular/angular.php
@@ -2,24 +2,24 @@
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
-$db_host = 'localhost'; // Database is installed on the PHPH server
-$db_user = 'lev'; // name to log in to MySQL
-$db_password = 'southhills#'; // password to login to MySQL
-$db_name = 'lev'; // name of the database within MySQL
-$conn = new mysqli($db_host, $db_user, $db_password, $db_name);
-if ($conn->connect_error)
-{
-  die("Connection Failed: " . $conn->connect_error);
+$db_host = 'localhost'; // database is installed on php server
+$db_user = 'lev'; // name to login to mysql
+$db_password = 'southhills#'; // password
+$db_name = 'lev'; //name of db
+$conn = new mysqli($db_host,$db_user,$db_password,$db_name);
+if ($conn->connect_error){
+  die("Connection failed: ". $conn->connect_error);
 }
+else{echo "Connection  Success";}
 
 $result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 
 $outp = "";
-while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+while($rows = $result->fetch_array(MYSQLI_ASSOC)) {
     if ($outp != "") {$outp .= ",";}
-    $outp .= '{"Name":"' . $rs["first_name"] . '",';
-    $outp .= '"Last_Name":"' . $rs["last_name"] . '",';
-    $outp .= '"City":"' . $rs["city"] . '"}';
+    $outp .= '{"Name":"' . $rows["first_name"] . '",';
+    $outp .= '"Last_Name":"' . $rows["last_name"] . '",';
+    $outp .= '"City":"' . $rows["city"] . '"}';
 }
 $outp ='{"records":['.$outp.']}';
 $conn->close();
--- angular/myctr.js
+++ angular/myctr.js
@@ -1,10 +1,10 @@
-app.controller("myCtrl", function ($scope, $http)
+app.controller("myctrl", function ($scope, $http)
 {
   $scope.firstName = "Lev";
   $scope.lastName = "t";
 
 
-  $http.get("getUsers.php")
+  $http.get("angular.php")
     .then(function (response)
     {
         $scope.myData = response.data.records;
