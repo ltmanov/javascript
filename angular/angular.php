39b3b5acb80213426b00e0ad94e18d93c15b079d
ltmanov <42806092+ltmanov@users.noreply.github.com>
2018-11-07 08:19:31 -0500

    a

--- angular/angular.php
+++ angular/angular.php
@@ -10,6 +10,7 @@ $conn = new mysqli($db_host,$db_user,$db_password,$db_name);
 if ($conn->connect_error){
   die("Connection failed: ". $conn->connect_error);
 }
+
 $result = $conn->query("SELECT first_name, last_name, city FROM angular_people");
 $outp ="";
 while($rows = $result->fetch_array(MYSQLI_ASSOC)) {
